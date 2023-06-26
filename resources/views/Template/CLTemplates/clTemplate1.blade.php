d4410319ab4dba394cd01f784dd64afce0fd8511
unknown <sg03056924122@gmail.com>
2023-06-26 22:12:42 +0500

    First commit

--- resources/views/Template/CLTemplates/clTemplate1.blade.php
+++ resources/views/Template/CLTemplates/clTemplate1.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- <link href="/Templates/template1.css" rel="stylesheet"> -->
-    <link href={{asset('css/Templates/CoverLetterTemplates/clTemplate1.css')}} rel="stylesheet">
+    <link href={{asset('css/Templates_css/CoverLetterTemplates/clTemplate1.css')}} rel="stylesheet">
 
     <title>Template1</title>
 </head>
